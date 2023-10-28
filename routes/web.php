cb997ba02f8a84790f5aaaac26ede155dd034fb7
thuytx03 <111112338+thuytx03@users.noreply.github.com>
2023-10-28 10:43:09 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use App\Http\Controllers\Client\HomeController;
 use App\Http\Controllers\Client\FavoriteController;
 use App\Http\Controllers\Admin\SeatController;
 use App\Http\Controllers\Admin\SeatTypeController;
+use App\Http\Controllers\Admin\ShowTimeController;
 
 Route::get('/', [HomeController::class, 'index'])->name('index');
 
